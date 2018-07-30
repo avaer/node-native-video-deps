2d3dccf69773c719d91615d6acd1b649b231ade3
Avaer Kazmer <a@modules.io>
2018-07-30 04:06:02 -0400

    Use unzipper module

--- postinstall.js
+++ postinstall.js
@@ -1,62 +1,87 @@
 const path = require('path');
 const fs = require('fs');
 const os = require('os');
-const unzpr = require('unzpr');
+const unzipper = require('unzipper');
 const rimraf = require('rimraf');
-const rs = fs.createReadStream(path.join(__dirname, 'lib.zip'));
 
-rs.on('open', () => {
-  const ws = rs.pipe(unzpr.Extract({
-    path: __dirname,
-  }));
-  ws.on('done', () => {
-    rimraf(path.join(__dirname, 'lib.zip'), err => {
-      if (err) {
-        throw err;
+['lib.zip', 'lib2.zip'].map(lib => {
+  const rs = fs.createReadStream(path.join(__dirname, lib));
+  rs.on('open', () => {
+    const ws = rs.pipe(unzipper.Extract({
+      path: __dirname,
+    }));
+    ws.on('close', () => {
+      rimraf(path.join(__dirname, lib), err => {
+        if (err) {
+          throw err;
+        }
+      });
+      const platform = os.platform();
+      switch (platform) {
+        case 'win32': {
+          ['macos', 'linux', 'android', 'ios'].forEach(p => {
+            rimraf(path.join(__dirname, 'lib', p), err => {
+              if (err) {
+                throw err;
+              }
+            });
+          });
+          rimraf(path.join(__dirname, 'lib2'), err => {
+            if (err) {
+              throw err;
+            }
+          });
+          break;
+        }
+        case 'darwin': {
+          ['windows', 'linux', 'android', 'ios'].forEach(p => {
+            rimraf(path.join(__dirname, 'lib', p), err => {
+              if (err) {
+                throw err;
+              }
+            });
+          });
+          rimraf(path.join(__dirname, 'lib2'), err => {
+            if (err) {
+              throw err;
+            }
+          });
+          break;
+        }
+        case 'linux': {
+          if (process.arch === 'x64') {
+            ['windows', 'macos', 'android', 'ios'].forEach(p => {
+              rimraf(path.join(__dirname, 'lib', p), err => {
+                if (err) {
+                  throw err;
+                }
+              });
+            });
+            rimraf(path.join(__dirname, 'lib2'), err => {
+              if (err) {
+                throw err;
+              }
+            });
+          } else {
+            rimraf(path.join(__dirname, 'lib'), err => {
+              if (err) {
+                throw err;
+              }
+            });
+          }
+          break;
+        }
+        default: throw new Error('unknown platform: ' + platform);
       }
     });
-    const platform = os.platform();
-    switch (platform) {
-      case 'win32': {
-        ['macos', 'linux'].forEach(p => {
-          rimraf(path.join(__dirname, 'lib', p), err => {
-            if (err) {
-              throw err;
-            }
-          });
-        });
-        break;
-      }
-      case 'darwin': {
-        ['win', 'linux'].forEach(p => {
-          rimraf(path.join(__dirname, 'lib', p), err => {
-            if (err) {
-              throw err;
-            }
-          });
-        });
-        break;
-      }
-      case 'linux': {
-        ['win', 'macos'].forEach(p => {
-          rimraf(path.join(__dirname, 'lib', p), err => {
-            if (err) {
-              throw err;
-            }
-          });
-        });
-        break;
-      }
-      default: throw new Error('unknown platform: ' + platform);
+  });
+  rs.on('error', err => {
+    if (err.code === 'ENOENT') {
+      process.exit(0);
+    } else {
+      throw err;
     }
   });
-});
-rs.on('error', err => {
-  if (err.code === 'ENOENT') {
-    process.exit(0);
-  } else {
-    throw err;
-  }
 });
 
 process.on('uncaughtException', err => {
