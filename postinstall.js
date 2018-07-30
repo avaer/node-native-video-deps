34a55f6d59084f39d71e4da62f0efb7d690b8495
Avaer Kazmer <a@modules.io>
2018-07-30 04:18:36 -0400

    Update postinstall script

--- postinstall.js
+++ postinstall.js
@@ -4,14 +4,14 @@ const os = require('os');
 const unzipper = require('unzipper');
 const rimraf = require('rimraf');
 
-['lib.zip', 'lib2.zip'].map(lib => {
-  const rs = fs.createReadStream(path.join(__dirname, lib));
+['lib', 'lib2'].map(lib => {
+  const rs = fs.createReadStream(path.join(__dirname, lib + '.zip'));
   rs.on('open', () => {
     const ws = rs.pipe(unzipper.Extract({
       path: __dirname,
     }));
     ws.on('close', () => {
-      rimraf(path.join(__dirname, lib), err => {
+      rimraf(path.join(__dirname, lib + '.zip'), err => {
         if (err) {
           throw err;
         }
@@ -29,7 +29,7 @@ const rimraf = require('rimraf');
           break;
         }
         case 'darwin': {
-          ['windows', 'linux', 'android', 'ios', 'arm64'].forEach(p => {
+          ['win', 'linux', 'android', 'ios', 'arm64'].forEach(p => {
             rimraf(path.join(__dirname, lib, p), err => {
               if (err) {
                 throw err;
@@ -40,7 +40,7 @@ const rimraf = require('rimraf');
         }
         case 'linux': {
           if (process.arch === 'x64') {
-            ['windows', 'macos', 'android', 'ios', 'arm64'].forEach(p => {
+            ['win', 'macos', 'android', 'ios', 'arm64'].forEach(p => {
               rimraf(path.join(__dirname, lib, p), err => {
                 if (err) {
                   throw err;
@@ -48,7 +48,7 @@ const rimraf = require('rimraf');
               });
             });
           } else if (process.arch === 'arm64') {
-            ['windows', 'macos', 'linux', 'android', 'ios'].forEach(p => {
+            ['win', 'macos', 'linux', 'android', 'ios'].forEach(p => {
               rimraf(path.join(__dirname, lib, p), err => {
                 if (err) {
                   throw err;
